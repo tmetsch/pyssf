f9fcf8559c7ac5489998109aaa906afebaff6648
tmetsch@platform.com <tmetsch@platform.com>
2010-09-22 13:43:04 +0200

    added stuff for future pypi deplyoment

--- setup.py
+++ setup.py
@@ -17,19 +17,31 @@
 # License along with this library; if not, write to the Free Software
 # Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
 # 
-
 from distutils.core import setup, Extension
 
-setup(name = 'Service Sharing Facility',
+setup(name = 'pyssf',
       version = '0.1',
-      license = 'LGPL',
       description = 'Service Sharing Facility',
+      license = 'LGPL',
+      keywords = 'REST,RESTful,OCCI,LSF,Platform,Cloud,Grid,Cluster,HPC',
       url = 'http://pyssf.sourceforge.net',
       packages = ['ssf', 'pyrest', 'pydrmaa'],
+      long_description = 'A set of Python modules to integrate you existing or future applications with Clouds, Grids and Clusters.',
       ext_package = 'pylsf',
       ext_modules = [Extension('_lsf', ['pylsf/lsf.i'],
                                include_dirs = ['/usr/include/python2.6'],
                                library_dirs = ['/opt/lsf/7.0/linux2.6-glibc2.3-x86_64/lib'],
                                libraries = ['c', 'nsl', 'lsf', 'bat', 'fairshareadjust', 'lsbstream'])],
       py_modules = ['pylsf.lsf'],
+      classifiers = ["Development Status :: 2 - Pre-Alpha",
+                     "License :: OSI Approved :: GNU Library or Lesser General Public License (LGPL)",
+                     "Operating System :: OS Independent",
+                     "Programming Language :: Python",
+                     "Topic :: Internet",
+                     "Topic :: Scientific/Engineering",
+                     "Topic :: Software Development",
+                     "Topic :: System :: Distributed Computing",
+                     "Topic :: Utilities",
+                     ],
      )
+
