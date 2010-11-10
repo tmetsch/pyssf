e6085170c62940e8645b1c1124eadbf7651dde09
tmetsch <none@none>
2010-11-10 16:31:51 +0100

    comments added

--- pyocci/service.py
+++ pyocci/service.py
@@ -23,6 +23,26 @@ Created on Nov 10, 2010
 @author: tmetsch
 '''
 
+import web
+
+def extract_http_data():
+    '''
+    Extracts all necesarry information from the HTTP envelop. Minimize the data
+    which is carried aroud inside of the service. Also ensures that the names
+    are always equal - When deployed in Apache the names of the Headers change.
+    '''
+    # FIXME: check for names when deployed in Apache...
+    result = {}
+    print web.ctx.env
+    # Cherrypy server naming...
+    if 'HTTP_CATEGORY' in web.ctx.env:
+        result['Category'] = web.ctx.env['HTTP_CATEGORY']
+    if 'HTTP_LINK' in web.ctx.env:
+        result['Link'] = web.ctx.env['HTTP_LINK']
+    if 'HTTP_X_OCCI_ATTRIBUTE' in web.ctx.env:
+        result['Attribute'] = web.ctx.env['HTTP_X_OCCI_ATTRIBUTE']
+    return result, repr(web.data())
+
 class HTTPVerbHandler(object):
     '''
     Handles basic HTTP operations. To achieve this it will make use of WSGI and
@@ -39,8 +59,18 @@ class HTTPVerbHandler(object):
         @param key: The Path of the URI.
         @type key: String
         '''
-        print('POST called on %s', key)
-        return 'OK'
+        headers, body = extract_http_data();
+        print(headers)
+        print(body)
+        # trigger actions
+
+        # mixins in loc N/A
+
+        # kind in loc (create new entity - res, link)
+
+        # loc = query (create a new mixin)
+
+        web.OK()
 
     def PUT(self, key):
         '''
@@ -49,6 +79,12 @@ class HTTPVerbHandler(object):
         @param key: The Path of the URI.
         @type key: String
         '''
+        # mixins in loc (add entity - res, link to mixin)
+
+        # kind in loc (create entity - res, link)
+
+        # loc = query (N/A)
+
         print('PUT called on %s', key)
 
     def GET(self, key):
@@ -58,6 +94,12 @@ class HTTPVerbHandler(object):
         @param key: The Path of the URI.
         @type key: String
         '''
+        # mixins in loc (return redirects to entities)
+
+        # kind in loc (return locations of entitires)
+
+        # loc = query (return listing of all categories)
+
         print('GET called on %s', key)
 
     def DELETE(self, key):
@@ -67,4 +109,10 @@ class HTTPVerbHandler(object):
         @param key: The Path of the URI.
         @type key: String
         '''
+        # mixins in loc (remove entity from mixin)
+
+        # kind in loc (delete entitiy)
+
+        # loc = query (delete user degined-mixin)
+
         print('DELETE called on %s', key)
