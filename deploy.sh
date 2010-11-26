50f9d7a0d303134cde470a52edcee03c89d7a0ba
tmetsch@platform.com <tmetsch@platform.com>
2010-11-26 18:24:09 +0100

    fixed issue

--- deploy.sh
+++ deploy.sh
@@ -51,7 +51,9 @@ case "$1" in
     ;;
 
   coverage)
-    nosetests --with-coverage --cover-html --cover-html-dir=docs/_build/html/cover --cover-erase --cover-package=pyocci
+    export PYTHONPATH=build/lib.linux-x86_64-2.6/pylsf/
+	export PYDRMAA_LIBRARY_PATH=/opt/lsf/7.0/linux2.6-glibc2.3-x86_64/lib/libdrmaa.so
+	nosetests --with-coverage --cover-html --cover-html-dir=docs/_build/html/cover --cover-erase --cover-package=pyocci
     export PYOCCI_STYLE_SHEET=`pwd`/misc/style.css
     nosetests --with-coverage --cover-html --cover-html-dir=docs/_build/html/cover --cover-package=pyocci
     rc=$?
