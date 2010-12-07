9d90818284012835ea63ff928c76e3730d39775b
tmetsch <none@none>
2010-12-07 17:29:31 +0100

    Added a skeleton whihc is OCCI infrastructure compatible

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@
 from distutils.core import setup, Extension
 
 setup(name = 'pyssf',
-      version = '0.2.3',
+      version = '0.2.4',
       description = 'Service Sharing Facility',
       license = 'LGPL',
       keywords = 'REST,RESTful,OCCI,LSF,Platform,Cloud,Grid,Cluster,HPC',
