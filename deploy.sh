32f48a9a37bd19e672bc2a81baf75ec9c8c8c5ff
tmetsch <none@none>
2011-01-11 13:27:49 +0100

    updated copyright years

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # 
-# Copyright (C) 2010 Platform Computing
+# Copyright (C) 2010-2011 Platform Computing
 # 
 # This library is free software; you can redistribute it and/or
 # modify it under the terms of the GNU Lesser General Public
