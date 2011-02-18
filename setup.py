8da4b756694090ceeef7f0aa58d846162a32fd3a
tmetsch <none@none>
2011-02-18 10:26:56 +0100

    minor fixes to CSS updated version info

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@
 from distutils.core import setup
 
 setup(name = 'pyssf',
-      version = '0.2.9',
+      version = '0.3.0',
       description = 'Service Sharing Facility',
       license = 'LGPL',
       keywords = 'REST,RESTful,OCCI,LSF,Platform,Cloud,Grid,Cluster,HPC',
